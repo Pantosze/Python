c0b87ba354905322a93572f94991947ed2337fc2
pantosze <pantosze@users.noreply.github.com>
2019-02-09 08:53:29 +0100

    Init, README

---
+++ PYTHON/main.py
@@ -0,0 +1,11 @@
+
+imie="Heniek"
+waga=100
+wiek=99
+print("mam na imie " + imie + " waze " + str(waga) + " i mam " + str(wiek) +"lat")
+produkty = ["mleko","chleb"]
+for s in produkty:
+    print(s)
+print(len(produkty))
+for idx in range(len(produkty)):
+    print ("idx:" + str(idx) + produkty[idx])
